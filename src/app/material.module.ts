e932ecced39f366fd4f067cf6a0ee6876c3e5cc1
Brutalvik <66142815+Brutalvik@users.noreply.github.com>
2022-06-19 02:50:58 -0600

    Added close sidenave on menu selection

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,9 +3,22 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-  imports: [MatButtonModule, MatToolbarModule, MatIconModule, MatSidenavModule],
-  exports: [MatButtonModule, MatToolbarModule, MatIconModule, MatSidenavModule],
+  imports: [
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+  ],
+  exports: [
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+  ],
 })
 export class MaterialModule {}
